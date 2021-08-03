c4f58453052fcfb391fd23ae80e9d61cc2925278
Andrey Rybkin <57178397+MisterRybkin@users.noreply.github.com>
2021-08-03 16:47:23 +0700

    Delete Suscription directory

--- Suscription/src/main/java/ru/rybkin/suscription/repositories/RepoSubscription.java
+++
@@ -1,7 +0,0 @@
-package ru.rybkin.suscription.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.rybkin.suscription.entity.Subscription;
-
-public interface RepoSubscription extends JpaRepository<Subscription, Integer> {
-}
